1b5a9258ab209d4aaa26f7cdefbd5906608020e0
jojobaa <joycarvalho59@gmail.com>
2022-11-21 00:20:26 -0300

    rota sign up

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import cors from "cors"
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import joi from 'joi'
+import bcrypt from "bcrypt"
 
 dotenv.config();
 
-const userSchema = joi.object({
-  name: joi.string().required()
-});
+// const userSchema = joi.object({
+//   name: joi.string().required()
+// });
 
-const messageSchema = joi.object({
-  to: joi.string().required(),
-  text: joi.string().required(),
-  type: joi.string().valid("message", "private_message").required()
-});
+// const messageSchema = joi.object({
+//   to: joi.string().required(),
+//   text: joi.string().required(),
+//   type: joi.string().valid("message", "private_message").required()
+// });
 
 const server = express();
 server.use(cors());
@@ -26,3 +27,18 @@ let db;
 await mongoClient.connect().then(() => {
   db = mongoClient.db("My-wallet");
 });
+
+server.post("/sign-up", async (req, res) => {
+    const user = req.body;
+    const hashPassword = bcrypt.hashSync(user.password, 10);
+  
+    try {
+      await db.collection("users").insertOne({ ...user, password: hashPassword });
+      res.sendStatus(201);
+    } catch (err) {
+      console.log(err);
+      res.sendStatus(500);
+    }
+  });
+
+server.listen(5000, () => console.log("Server in port 5000"));
